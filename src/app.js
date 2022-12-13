60c829dc38dc22fad93ad2092b2a2753a17d4fc1
Sebastián Cardona <sebascarmun@gmail.com>
2022-12-12 20:33:40 -0400

    mejoras

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 
 //⬇⬇⬇ Options ⬇⬇⬇
 let pronoun = ['the','our'];
-let adj = ['great', 'big' ];
+let adj = ['great', 'big','little' ];
 let noun = ['jogger','racoon'];
 let topLvldomain = ['.com','.net','.co','.mx','.tv']
 
 //⬇⬇⬇ Code ⬇⬇⬇
-for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-            for (let l = 0; l < topLvldomain.length; l++) {
-                console.log (`${pronoun[i]}${adj[j]}${noun[k]}${topLvldomain[l]}`)
+for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+            for (let t = 0; t < topLvldomain.length; t++) {
+                console.log (`${pronoun[p]}${adj[a]}${noun[n]}${topLvldomain[t]}`)
             }
         }
-    }
-    
+    }    
 }
